54dc41474fad6d627338e2caf9a4e18c5924b3cd
Rose Luna <r.os.el.u.na.041@gmail.com>
2026-07-01 04:28:34 +0000

    fix(vendor): remove duplicated tab label, keep singular "Statement" (#32)

--- frontend/src/pages/purchases/vendors/detail.tsx
+++ frontend/src/pages/purchases/vendors/detail.tsx
@@ -331,7 +331,6 @@ export default function VendorDetailPage() {
               : "text-gray-500 hover:text-gray-700"
               }`}
           >
-            Statements
             Statement
             {mainTab === "statements" && (
               <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-priori-purple" />
